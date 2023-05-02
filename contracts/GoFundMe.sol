7421ca779698fa7df219984fc550394ba7b9c493
Colin Stark <collinscodes@gmail.com>
2023-05-03 00:33:12 +0100

    fix
    Hello [web3Developer12](https://github.com/web3Developer12) 😄
    
    # Improvements
    1. I removed some variables and replaced them with `totalRaised` as the `fundsregistered` and the `funds` array in the `campaign` struct could be replaced by it. i also had to rewrite the functions with those variables to reflect the new 
    2. I removed the global variable `_pointer_campaign` 
    3. I changed the `block.number` to `block.timestamp`
    4. Added a function to ensure that there was no money in the contract before it is destroyed by the owner.
    5. I implemented the doxygen style commenting for better understanding and clarification
    
    
    # Suggestion
    i see no reason for calling the `selfdestruct` if the owner doesn't want to own the contract, it should be transferred which will involve implementing a transfer of onwer function like this
    
    ```
    address immutable owner;
    
    constructor()
    {
    owner = msg.sender;
    }
    
    
    modifier onlyOwner{
    require(msg.sender == owner, "Not the owner of the contract);
    }
    
    function transferOwnership(address newOwner) external onlyOwner {
        owner = newOwner;
    }
    ```

--- contracts/GoFundMe.sol
+++ contracts/GoFundMe.sol
@@ -1,3 +1,11 @@
+/**
+
+@title GoFundMe
+@dev A smart contract that allows users to create and contribute to fundraising campaigns
+@notice This contract is licensed under the MIT license
+*/
+
+
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
@@ -22,8 +30,7 @@ contract GoFundMe{
         string  description;
         string  web3storage;
         string  web3storagePath;
-        uint256 fundsRegister;
-        campaign_funds[] funds;
+        uint256 totalRaised;
     }
 
     mapping    (string => campaign) campaigns;
@@ -41,70 +48,103 @@ contract GoFundMe{
         owner = msg.sender;
     }
 
+    /**
+        * @dev Check if a campaign exists
+        * @param _key The title of the campaign
+        * @return true if the campaign exists, false otherwise
+    */
+
     function exist(string calldata _key) internal view returns(bool){
         return campaigns[_key].startDate > 0 ? true:false;
     }
 
+    /**
+        * @dev Create a new fundraising campaign
+        * @param _goal The fundraising goal in wei
+        * @param _category The category of the campaign
+        * @param _title The title of the campaign
+        * @param _description The description of the campaign
+        * @param _cid The IPFS CID of the campaign
+        * @param _cidPath The IPFS CID path of the campaign
+        * @return true if the campaign is successfully created
+    */
     function launch(
-        uint256 _goal,string calldata _category,string calldata _title,string calldata _description,string calldata _cid,string calldata _cidPath
-    ) external returns(bool){
+    uint256 _goal,string calldata _category,string calldata _title,string calldata _description,string calldata _cid,string calldata _cidPath
+) external returns(bool){
 
-        require(exist(_title) == false,"campaign already exist");
-        
-        _pointer_campaign.beneficiary = msg.sender;
-        _pointer_campaign.startDate   = block.number;
-        _pointer_campaign.endDate     = block.number;
-        _pointer_campaign.goal        = _goal;
-        _pointer_campaign.category    = _category;
-        _pointer_campaign.title       = _title;
-        _pointer_campaign.description = _description;
-        _pointer_campaign.web3storage = _cid;
-        _pointer_campaign.web3storagePath = _cidPath;
+    require(exist(_title) == false,"campaign already exist");
 
-        campaigns[_title] = _pointer_campaign;
-        title_ledger.push(_title);
+    campaign memory newCampaign;
+    newCampaign.beneficiary = msg.sender;
+    newCampaign.startDate   = block.timestamp;
+    newCampaign.endDate     = block.timestamp;
+    newCampaign.goal        = _goal;
+    newCampaign.category    = _category;
+    newCampaign.title       = _title;
+    newCampaign.description = _description;
+    newCampaign.web3storage = _cid;
+    newCampaign.web3storagePath = _cidPath;
 
-        return true;
-    }
+    campaigns[_title] = newCampaign;
+    title_ledger.push(_title);
 
+    return true;
+}
+
+    /**
+        * @dev Contribute to a fundraising campaign
+        * @param _title The title of the campaign
+    */
     function contribute(string calldata _title) external payable{
 
         require(exist(_title) == true,"campaign don't exist");
         require(msg.value > 0,"amount must be greater than 0");
-        require(
-            (msg.value / (10 ** 18)) > campaigns[_title].goal == false,
-            "amount exceed the campaign goal"
-        );
-
+        require(msg.value + campaigns[_title].totalRaised <= campaigns[_title].goal, "Amount exceed the campaign goal");
         celoToken.approve(address(this) ,msg.value);
-        celoToken.transfer(address(this),msg.value);
+        celoToken.transferFrom(msg.sender, address(this), msg.value);        
+
+        campaigns[_title].totalRaised += msg.value;
         
-        campaign_funds memory donation =  campaign_funds(
-            msg.sender,msg.value,block.number
-        );
-        campaigns[_title].fundsRegister +=1;
-        campaigns[_title].funds.push(donation);
     }
+
+    /**
+        @dev Allows the beneficiary of a campaign to claim the funds if the goal has been achieved.
+        @param _title The title of the campaign.
+        Requirements:
+        The campaign must exist.
+        The caller must be the beneficiary of the campaign.
+        The total amount raised must be equal to the campaign goal.
+        Approval must be granted to this contract to transfer the funds on behalf of the user.
+    */
 
     function claim(string calldata _title) external payable{
 
         require(exist(_title) == true,"campaign don't exist");
         require(campaigns[_title].beneficiary == msg.sender,"you are not the beneficiary");
         require(
-            campaigns[_title].fundsRegister == campaigns[_title].goal,
+            campaigns[_title].totalRaised == campaigns[_title].goal,
             "the goal is not yet achieved"
         );
-        uint256 _totalRaised = campaigns[_title].fundsRegister;
-        delete campaigns[_title].funds;
+        uint256 _totalRaised = campaigns[_title].totalRaised;
         celoToken.approve(address(this), _totalRaised);
         celoToken.transferFrom(address(this),campaigns[_title].beneficiary,_totalRaised);
         
     }
 
+    /**
+        @dev Returns an array of campaign titles that have been launched.
+        @return An array of strings representing the campaign titles.
+    */
+
     function getKeys() external view returns(string[] memory){
         return title_ledger;
     }
 
+    /**
+        @dev Returns the campaign data associated with the given _key.
+        @param _key The key of the campaign to fetch data for.
+        @return The campaign data stored in a campaign struct.
+    */
     function fetchData(string calldata _key) external view returns(campaign memory){
         return campaigns[_key];
     }
@@ -113,7 +153,16 @@ contract GoFundMe{
 
     }
 
+    /**
+        @dev Destroys the contract and transfers any remaining funds to the owner.
+        Requirements:
+        The contract must have no remaining funds.
+        Only the owner can call this function.
+    */
+
     function destroy() external {
+        uint256 balance = address(this).balance;
+        require(balance <= 0, "No remaining funds");
         require(msg.sender == owner);
         selfdestruct(payable(msg.sender));
     }
