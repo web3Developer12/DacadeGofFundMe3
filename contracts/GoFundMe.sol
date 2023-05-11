6a6fff7c1cf15a14475f9fdb792cd479ffa7eb11
Saimplice Ruud Christopher <113187996+web3Developer12@users.noreply.github.com>
2023-05-11 13:25:26 -0400

    Update GoFundMe.sol

--- contracts/GoFundMe.sol
+++ contracts/GoFundMe.sol
@@ -2,16 +2,13 @@
 
 pragma solidity ^0.8.17;
 
-// Imports the ERC20 token standard from the OpenZeppelin library.
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 contract GoFundMe {
     /*
         This defines a campaign struct that will store information about 
         each fundraising campaign launched using the GoFundMe contract.
     */
     struct campaign {
-
+        bytes32 hash;
         /*The owner of the campaign*/
         address beneficiary    ;
         /*The starting date of the campaign*/
@@ -34,25 +31,33 @@ contract GoFundMe {
         string  web3storage    ;
         /*The link of the path in ipfs*/
         string  web3storagePath;
+        bool    closed         ;
     }
 
-    /*Mapping from campaign hash to their corresponding campaign struct*/
     mapping    (bytes32 => campaign) campaigns;
+    bytes32[]  hashes;
+    address[]  allowedToClaim;
+
 
     /*Register hash off all campaings in the blockchain */
-    bytes32[]   private hash_register  ;
     address     private immutable owner;
 
-    /*Instance of the CELO-ERC20 token contract.*/
-    IERC20 private immutable ERC = IERC20(0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9);
 
-    bytes32 public lastHash;
+    
 
     constructor(){
         /*owner of the smart contract*/
         owner = msg.sender;
     }
 
+    function isAllowedToClaim() internal view returns(bool){
+        for(uint256 i=0;i<allowedToClaim.length;++i){
+            if(allowedToClaim[i] == msg.sender){
+                return true;
+            }
+        }
+        return false;
+    }
     /*Check if the campaign already exist*/
     function exist(bytes32 key) internal view returns(bool){
         return campaigns[key].startDate > 0 ? true:false;
@@ -74,39 +79,51 @@ contract GoFundMe {
         require(exist(hash) == false,"campaign already exist");
 
         campaign memory _newCampaign = campaign(
-            msg.sender,block.timestamp,block.timestamp + 22 weeks,
-            _goal,0,0,_category,_title,_description,_cid,_cidPath
+            hash,msg.sender,block.timestamp,block.timestamp + 22 weeks,
+            _goal * (10 ** 18),0,0,_category,_title,_description,_cid,_cidPath,false
         );
 
         campaigns[hash] = _newCampaign;
-        lastHash = hash;
+        hashes.push(hash);
+        allowedToClaim.push(msg.sender);
         return hash;
     }
 
     /**This function raise a specific campaign */
     function contribute(bytes32 key) external payable{
 
-        require(msg.value > 0,"amount must be greater than 0");
-        require(exist(key) == true,"campaign don't exist"    );
-        require(
-            msg.value > campaigns[key].goal == false,
-            "amount exceed the campaign goal"
-        );
+        require(exist(key) == true);
+        require(msg.value > 0);
 
-        ERC.transferFrom(msg.sender,address(this),msg.value);
-        campaigns[key].donations +=1;
+        uint256 remaining = campaigns[key].goal - campaigns[key].celoRaised;
+        require(msg.value <= remaining,"amount exceed goal");
+        
+        campaigns[key].donations  += 1;
+        campaigns[key].celoRaised += msg.value;
+    }
+    
+    /*Only beneficiary listed can withdraw funds */
+    modifier onlyAllowedToClaim {
+      require(isAllowedToClaim());
+      _;
     }
 
     /**This function claim the total celo raised for a campaign */
-    function claim(bytes32 key) external payable{
+    function claim(bytes32 key) external onlyAllowedToClaim{
 
-        require(exist(key) == true,"campaign don't exist");
-        require(campaigns[key].beneficiary == msg.sender,"you are not the beneficiary");
-        require(
-            campaigns[key].celoRaised == campaigns[key].goal,
-            "the goal is not yet achieved"
-        );
-        ERC.transferFrom(address(this),campaigns[key].beneficiary,campaigns[key].celoRaised);
+        require(exist(key) == true && campaigns[key].closed == false);
+        require(campaigns[key].beneficiary == msg.sender);
+        (bool s,) = (msg.sender).call{value:campaigns[key].celoRaised}("");
+        require(s);
+        campaigns[key].closed = true;
+    }
+
+    function getHashes() external view returns(bytes32[] memory){
+        return hashes;
+    }
+
+    function readByHashes(bytes32 key) external view returns(campaign memory){
+        return campaigns[key];
     }
 
     receive() external payable{}
